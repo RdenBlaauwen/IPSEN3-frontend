ff5127b12ea379ff2202a04063b10ff303dd650d
Reza Naser <rezanm2@gmail.com>
2017-12-15 14:23:45 +0100

    Inlog scherm werkt

---
+++ src/app/sprints/edit-sprints/edit-sprints.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-sprints',
+  templateUrl: './edit-sprints.component.html',
+  styleUrls: ['./edit-sprints.component.css']
+})
+export class EditSprintsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
