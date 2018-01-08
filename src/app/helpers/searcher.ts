2f40e24e745e019d0bb0e53b4c98762e70621a01
Unknown <rdenblaauwen@gmail.com>
2018-01-08 21:40:28 +0100

    Filtering is coming along.

--- src/app/helpers/searcher.ts
+++ src/app/helpers/searcher.ts
@@ -1,5 +1,16 @@
 export class Searcher {
-    public contains(input: string, substring: string): boolean {
-        return (input.indexOf(substring) >= 0) ? true : false;
+    public hasString(input: string, substring: string): boolean {
+        if(substring!=null&&substring!=""){
+            return (input.indexOf(substring) >= 0) ? true : false;
+        }
+        return false;
+    }
+    public hasInt(input: number, substring: number): boolean {
+        if(substring!=null){
+            let parsedInput=input.toString();
+            let parsedSubString = substring.toString();
+            return (parsedInput.indexOf(parsedSubString) >= 0) ? true : false;
+        }
+        return false;
     }
 }
